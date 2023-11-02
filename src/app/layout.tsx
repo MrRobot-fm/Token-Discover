0a4fef329d19f0926d677b139b852430ddccdcb3
MrRobot-fm <federicomigliore87@gmail.com>
2023-11-02 14:49:09 +0100

    feat(chakra-init): chakra installation and configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { ChakraProviders } from "@/lib/chakra/provider/ChakraProvider";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,7 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <ChakraProviders>{children}</ChakraProviders>
+      </body>
     </html>
   );
 }
