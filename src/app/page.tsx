6745b5190bb6ccd46fe6490db050fb0ecb92dd94
MrRobot-fm <federicomigliore87@gmail.com>
2023-11-02 20:05:01 +0100

    feat(colors): color configuration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div>
+    <div className="bg-background-grey">
       <h1 className="font-workSans text-red-600">ciao</h1>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,21 @@ const config: Config = {
         workSans: ["var(--font-works-sans)"],
         spaceMono: ["var(--font-space-mono)"],
       },
+      colors: {
+        background: {
+          black: "#2B2B2B",
+          grey: "#3B3B3B",
+        },
+        label: {
+          grey: "#858584",
+          white: "#FFF",
+        },
+        callToAction: "#A259FF",
+      },
+      backgroundImage: {
+        purpleBlue: "linear-gradient(128deg, #A259FF 49.75%, #377DF7 136.56%)",
+        purpleRed: "linear-gradient(101deg, #A259FF 13.57%, #FF6250 97.65%)",
+      },
     },
   },
   plugins: [],
