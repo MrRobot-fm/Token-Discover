1aba7cc38a85f2607dda64970caf0f45ae7b12ef
MrRobot-fm <federicomigliore87@gmail.com>
2024-01-05 11:01:10 +0100

    feat(loading-ui): loading spinner created and loading ui implemented

---
+++ src/app/loading.tsx
@@ -0,0 +1,5 @@
+import { LoadingSpinner } from "@/components/atoms/LoadingSpinner/LoadingSpinner";
+
+export default function Loading() {
+  return <LoadingSpinner />;
+}
--- src/utils/providers/query-param-provider.tsx
+++ src/utils/providers/query-param-provider.tsx
@@ -1,15 +1,11 @@
 "use client";
 
 import NextAdapterApp from "next-query-params/app";
-import { type ReactNode, Suspense } from "react";
+import { type ReactNode } from "react";
 import { QueryParamProvider } from "use-query-params";
 
 export const QueryParamsProvider = ({ children }: { children: ReactNode }) => {
   return (
-    <Suspense fallback={<>Loading</>}>
-      <QueryParamProvider adapter={NextAdapterApp}>
-        {children}
-      </QueryParamProvider>
-    </Suspense>
+    <QueryParamProvider adapter={NextAdapterApp}>{children}</QueryParamProvider>
   );
 };
