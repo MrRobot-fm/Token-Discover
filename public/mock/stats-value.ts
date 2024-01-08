d0d441977f456bda91b85c182c59bfb31acc4d55
MrRobot-fm <federicomigliore87@gmail.com>
2024-01-08 12:26:07 +0100

    refactor(stats-card): stats value changed

--- public/mock/stats-value.ts
+++ public/mock/stats-value.ts
@@ -1,14 +1,14 @@
 
 export const statsValue=[
     { id:1,
-        stat:240,
+        stat:240000,
         label:'total sale'
     },
     { id:2,
-        stat:100,
-        label:'actions'
+        stat:20000,
+        label:'collections'
     },{ id:3,
-        stat:500,
-        label:'artist'
+        stat:80000,
+        label:'NFTs'
     }
 ]
