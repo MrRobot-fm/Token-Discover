b97596de07dc24600b289720b97d8801c0fc11fb
MrRobot-fm <federicomigliore87@gmail.com>
2024-01-08 17:33:18 +0100

    refactor(mobile-padding): mobile padding changed

--- src/app/contact-me/contact-me.styles.ts
+++ src/app/contact-me/contact-me.styles.ts
@@ -1,6 +1,6 @@
 export const styles = {
   contactMe_section:
-    "flex min-h-[calc(100vh-8.6rem)] w-full flex-col border-t border-t-background-grey base:gap-[4rem] base:px-[3rem] md:px-[10rem] lg:items-center justify-center lg:gap-[6rem] lg:px-[5rem] base:py-[5rem] lg:py-0",
+    "flex min-h-[calc(100vh-8.6rem)] w-full flex-col border-t border-t-background-grey base:gap-[4rem] base:px-[2rem] md:px-[10rem] lg:items-center justify-center lg:gap-[6rem] lg:px-[5rem] base:py-[5rem] lg:py-0",
   contactMe_section_container:
     "flex lg:flex-row base:flex-col base:gap-[5rem] h-fit w-full max-w-[144rem] flex-wrap lg:gap-[11rem] xl:gap-[18rem]",
   contactMe_section_container_flex1: "flex w-full flex-1",
--- src/app/discover/layout.tsx
+++ src/app/discover/layout.tsx
@@ -13,7 +13,7 @@ export default function DiscoverPagesLayout({
       <Section
         title="Discover our NFTs and Collections"
         subTitle="Browse through top sold and trending NTFs and our weekly top collection."
-        sectionStyles="base:px-[3rem] md:px-[10rem] lg:px-[5rem] base:py-[3.5rem] md:py-[7rem]"
+        sectionStyles="base:px-[2rem] md:px-[10rem] lg:px-[5rem] base:py-[3.5rem] md:py-[7rem]"
         titleProps={{
           titleStyles: "base:heading3 md:heading2",
           subtitleStyles: "lowercase first-letter:uppercase font-normal",
