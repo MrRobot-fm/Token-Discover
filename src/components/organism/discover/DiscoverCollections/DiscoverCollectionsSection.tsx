9b643dd3e89e480247ee037bc7687f80bf59c3db
MrRobot-fm <federicomigliore87@gmail.com>
2024-01-08 12:18:13 +0100

    fix(genral): general fix

--- src/components/organism/discover/DiscoverCollections/DiscoverCollectionsSection.tsx
+++ src/components/organism/discover/DiscoverCollections/DiscoverCollectionsSection.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import { useForm } from "react-hook-form";
-import { useSearchParams } from "@/hooks/use-search-params";
+import { useSearchTokens } from "@/hooks/use-search-tokens";
 import { useGetCollectionsByName } from "@/api/collections/hooks/use-get-collections-by-name";
 import { SearchBar } from "@/components/atoms/Forms/SearchBar/SearchBar";
 import { TopNftSoldCard } from "@/components/molecules/cards/TopNftSoldCard/TopNftSoldCard";
@@ -9,19 +8,7 @@ import { DiscoverIndexList } from "../DiscoverIndexList/DiscoverIndexList";
 import { useDiscoverCollections } from "./use-discover-collections";
 
 export const DiscoverCollectionsSection = () => {
-  const { register, watch, handleSubmit, reset } = useForm<{
-    collectionName: string;
-  }>({
-    defaultValues: {
-      collectionName: "",
-    },
-  });
-
-  const { search, handleSearch } = useSearchParams({
-    formValue: watch("collectionName"),
-    defaultParam: "bored",
-    reset,
-  });
+  const { search, handleSearch, register, handleSubmit } = useSearchTokens();
 
   const { data, isLoading } = useGetCollectionsByName({
     collectionName: search || "bored",
@@ -32,7 +19,7 @@ export const DiscoverCollectionsSection = () => {
   return (
     <div className="w-full py-[6rem] base:space-y-[4rem] xl:space-y-[6rem]">
       <SearchBar
-        name="collectionName"
+        name="searchValue"
         placeholder="Search your favorite collections"
         register={register}
         onSubmit={handleSubmit(handleSearch)}
--- src/utils/providers/tanstack-query-provider.tsx
+++ src/utils/providers/tanstack-query-provider.tsx
@@ -23,7 +23,9 @@ export default function TanstackQUeryProvider({
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={false} />
+      {process.env.NODE_ENV === "development" && (
+        <ReactQueryDevtools initialIsOpen={false} />
+      )}
     </QueryClientProvider>
   );
 }
