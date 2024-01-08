f53b920dd127c7df8ba093cfab07746f2f744123
MrRobot-fm <federicomigliore87@gmail.com>
2024-01-08 12:50:47 +0100

    fix(remove-depracated-items): remove unnecessary file and constants

--- src/utils/validation/forms-validation.ts
+++ src/utils/validation/forms-validation.ts
@@ -1,5 +1,5 @@
 import type { Message, RegisterOptions, ValidationRule } from "react-hook-form";
-import { EMAIL_REGEX, PASSWORD_REGEX } from "@/utils/constants/regex";
+import { EMAIL_REGEX } from "@/utils/constants/regex";
 import { INPUT_MIN_LENGTH } from "@/utils/constants/validation";
 
 export const sharedFormValidation: {
@@ -23,15 +23,6 @@ export const emailFormValidation: RegisterOptions = {
   pattern: {
     value: EMAIL_REGEX,
     message: "Please enter a valid email",
-  },
-  ...sharedFormValidation,
-};
-
-export const passwordFormValidation: RegisterOptions = {
-  pattern: {
-    value: PASSWORD_REGEX,
-    message:
-      "Password must contain one lowercase letter, one number and one special character",
   },
   ...sharedFormValidation,
 };
