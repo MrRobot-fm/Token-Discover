ba5bf8f918e4141a128495298721f958c5ddb104
MrRobot-fm <federicomigliore87@gmail.com>
2024-01-08 16:58:55 +0100

    refactor(nft-details-info): insert vieport conditionl rendering for contract address

--- src/components/atoms/navbar/NavbarLogo/NavbarLogo.tsx
+++ src/components/atoms/navbar/NavbarLogo/NavbarLogo.tsx
@@ -1,13 +1,16 @@
+import Link from "next/link";
 import { Icon } from "@/components/atoms/Icon/Icon";
 
 export const NavLogo = ({ text }: { text: string }) => {
   return (
-    <div className="flex items-start justify-start gap-[1rem]">
-      <Icon
-        name="siteLogo"
-        className="h-[3.2rem] w-[3.2rem] text-callToAction"
-      />
-      <span className="heading5 font-bold text-white">{text}</span>
-    </div>
+    <Link href="/">
+      <div className="flex items-start justify-start gap-[1rem]">
+        <Icon
+          name="siteLogo"
+          className="h-[3.2rem] w-[3.2rem] text-callToAction"
+        />
+        <span className="heading5 font-bold text-white">{text}</span>
+      </div>
+    </Link>
   );
 };
