aed7bdfaede0016733360e96371ba2b1584b2e10
MrRobot-fm <federicomigliore87@gmail.com>
2024-02-02 10:15:21 +0100

    perf(main): remove image optimization for limit exeded on vercel production

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: { 
+  unoptimized: true,
   remotePatterns: [
   {protocol:"https",  hostname:"lh3.googleusercontent.com"},
   {protocol:"https",hostname:"i.seadn.io"},
